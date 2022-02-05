799badff0ff395b4d9264cbd920afdbc62c147c0
Hrishikesh rai <hrishikesh.rai@glance.com>
2022-02-06 00:45:07 +0530

    twoSum soln

---
+++ twoSum.js
@@ -0,0 +1,17 @@
+
+const twoSum = (list, target) => {
+    const compliment = {}
+    for(let i = 0; i < list.length; i++) {
+        if(compliment[target - list[i]] !== undefined) {
+            console.log([compliment[target - list[i]],i])
+            return [i,compliment[target - list[i]]]
+            
+        }else {
+            compliment[list[i]] = i
+        }
+    }
+}
+
+twoSum([2,7,11,15],9);
+
+const a = {2: 0,}
